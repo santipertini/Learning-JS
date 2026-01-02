2279070c72e3edb3008c0444b1cbc0a95fc446a9
BiagoDev <pertiniperto@gmail.com>
2026-01-02 17:58:54 -0300

    Update and rename Main work to Main work.js
    Much better organization

---
+++ Works Cisco Level one/Main work.js
@@ -0,0 +1,35 @@
+let contacts = [{
+    name: "Maxwell Wright",
+    phone: "(0191) 719 6495",
+    email: "Curabitur.egestas.nunc@nonummyac.co.uk"
+}, {
+    name: "Raja Villarreal",
+    phone: "0866 398 2895",
+    email: "posuere.vulputate@sed.com"
+}]; //Previous contacts
+let options= prompt("1- FIRST CONTACT. 2- LAST CONTACT. 3- NEW CONTACT");
+switch(options){
+    case "1":
+    alert("Name: " + contacts[0].name + "\n" +
+    "Phone: " + contacts[0].phone + "\n" +
+    "Email: " + contacts[0].email); //Displays the first contact information in the console
+    break;
+
+    case "2":
+        alert("Name: " + contacts[1].name + "\n" +
+        "Phone: " + contacts[1].phone + "\n" +
+        "Email: " + contacts[1].email);
+         break; //Displays the last contact information in the console
+    case "3":
+    let name = prompt("Write his name;");
+    let phone = prompt("Write his phone number:");
+    let email = prompt ("Write his email"); //Asks for the new contact information
+    contacts[2] = {
+      name: name,
+      phone: phone,
+      email: email
+    }; //Saves this new information
+    alert("Your new contact:" + "\n" + "His name is: " + contacts[2].name + "\n" +
+    "His phone number is: " + contacts[2].phone + "\n" +
+    "His email is: " + contacts[2].email);
+} //Displays the new contact information
