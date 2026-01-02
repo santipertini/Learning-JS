bc66be7ae3e1b93c7abdc3acba61424f0250049c
BiagoDev <pertiniperto@gmail.com>
2026-01-02 17:56:41 -0300

    Update Main Act.js
    A more friendly code and some comments

--- Main Act.js
+++ Main Act.js
@@ -6,30 +6,30 @@ let contacts = [{
     name: "Raja Villarreal",
     phone: "0866 398 2895",
     email: "posuere.vulputate@sed.com"
-}];
+}]; //Previous contacts
 let options= prompt("1- FIRST CONTACT. 2- LAST CONTACT. 3- NEW CONTACT");
 switch(options){
     case "1":
     alert("Name: " + contacts[0].name + "\n" +
     "Phone: " + contacts[0].phone + "\n" +
-    "Email: " + contacts[0].email);
+    "Email: " + contacts[0].email); //Displays the first contact information in the console
     break;
 
     case "2":
         alert("Name: " + contacts[1].name + "\n" +
         "Phone: " + contacts[1].phone + "\n" +
         "Email: " + contacts[1].email);
-         break;
+         break; //Displays the last contact information in the console
     case "3":
-    let name = prompt("Name;");
-    let phone = prompt("Phone:");
-    let email = prompt ("Email");
+    let name = prompt("Write his name;");
+    let phone = prompt("Write his phone number:");
+    let email = prompt ("Write his email"); //Asks for the new contact information
     contacts[2] = {
       name: name,
       phone: phone,
       email: email
-    };
-    alert("Name: " + contacts[2].name + "\n" +
-    "Phone: " + contacts[2].phone + "\n" +
-    "Email: " + contacts[2].email);
-}
+    }; //Saves this new information
+    alert("Your new contact:" + "\n" + "His name is: " + contacts[2].name + "\n" +
+    "His phone number is: " + contacts[2].phone + "\n" +
+    "His email is: " + contacts[2].email);
+} //Displays the new contact information
