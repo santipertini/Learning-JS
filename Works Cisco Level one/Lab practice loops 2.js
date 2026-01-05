eb0c59041afbac229b715026ad128264d3f32ca3
BiagoDev <pertiniperto@gmail.com>
2026-01-05 13:42:55 -0300

    Update Lab practice loops 2.js

--- Works Cisco Level one/Lab practice loops 2.js
+++ Works Cisco Level one/Lab practice loops 2.js
@@ -1,1 +1,6 @@
-
+alert("Welcome to the list that writes your numbers from de biggest to the smallest by steps of 10");
+let max = prompt("Choose the biggest number on the list: ");
+let min = prompt("Choose the smallest number on the list: ");
+for(i=max;i>min;i-=10){
+    console.log(i);
+}
