6e8b3a51f698ad6435c72c978ad4fc2fe7820e4e
BiagoDev <pertiniperto@gmail.com>
2026-01-11 17:20:17 -0300

    Update Contact list ahora si xd.js

--- Works Cisco Level one/Contact list.js
+++ Works Cisco Level one/Contact list.js
@@ -16,55 +16,68 @@ let contacts = [
   }
 ];
 
+function showContact(contact) {
+  alert(
+    "Name: " + contact.name + "\n" +
+    "Phone: " + contact.phone + "\n" +
+    "Email: " + contact.email
+  );
+}
+
+function firstContact() {
+  showContact(contacts[0]);
+}
+
+function lastContact() {
+  showContact(contacts[contacts.length - 1]);
+}
+
+function allContacts() {
+  for (let i = 0; i < contacts.length; i++) {
+    showContact(contacts[i]);
+  }
+}
+
+function newContact() {
+  let name = prompt("Write his name:");
+  let phone = prompt("Write his phone number:");
+  let email = prompt("Write his email");
+
+  contacts[contacts.length] = {
+    name: name,
+    phone: phone,
+    email: email
+  };
+
+  alert(
+    "Your new contact:\n" +
+    "His name is: " + contacts[contacts.length - 1].name + "\n" +
+    "His phone number is: " + contacts[contacts.length - 1].phone + "\n" +
+    "His email is: " + contacts[contacts.length - 1].email
+  );
+}
+
 let options = prompt("1- FIRST CONTACT. 2- LAST CONTACT. 3- ALL CONTACTS. 4- NEW CONTACT. 5- QUIT");
 
 switch (options) {
   case "1":
-    alert(
-      "Name: " + contacts[0].name + "\n" +
-      "Phone: " + contacts[0].phone + "\n" +
-      "Email: " + contacts[0].email
-    );
+    firstContact();
     break;
 
   case "2":
-    alert(
-      "Name: " + contacts[contacts.length - 1].name + "\n" +
-      "Phone: " + contacts[contacts.length - 1].phone + "\n" +
-      "Email: " + contacts[contacts.length - 1].email
-    );
+    lastContact();
     break;
 
   case "3":
-    for (let i = 0; i < contacts.length; i++) {
-      alert(
-        "Name: " + contacts[i].name + "\n" +
-        "Phone: " + contacts[i].phone + "\n" +
-        "Email: " + contacts[i].email
-      );
-    }
+    allContacts();
     break;
 
   case "4":
-    let name = prompt("Write his name:");
-    let phone = prompt("Write his phone number:");
-    let email = prompt("Write his email");
-
-    contacts[contacts.length] = {
-      name: name,
-      phone: phone,
-      email: email
-    };
-
-    alert(
-      "Your new contact:\n" +
-      "His name is: " + contacts[contacts.length - 1].name + "\n" +
-      "His phone number is: " + contacts[contacts.length - 1].phone + "\n" +
-      "His email is: " + contacts[contacts.length - 1].email
-    );
+    newContact();
     break;
-    case "5":
-        return 0;
+
+  case "5":
+    return 0;
 
   default:
     alert("Invalid option");
